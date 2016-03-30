e6f040aff1b5edc30e2fb9dee9b6cdc2a8f6ff68
lemichel93 <xuan1811@yahoo.com>
2016-03-30 14:01:07 -0700

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,8 +1,31 @@
 define users:: managed_user (
 
+  $username = $title,
+  $uid = undef,
+  $groupname - undef,
+  $homedir = "/home/${title}",
+
 ) {
+  File {
+    owner => $username,
+    group => $groupname,
+    mode => '0644',
+    }
+    
+  user {$title:
+    ensure => present,
+    uid => $uid,
+    gid => $groupname,
+    home => $homedir,
+    name => $username,
+    }
+    
+  group {$title:
+    ensure => present,
+    name => $groupname,
+    }
+  file {[$homedir, "${homedir}/.ssh" ]:
+    ensure => directory
+    }
+  }
 
-
-
-
-}
